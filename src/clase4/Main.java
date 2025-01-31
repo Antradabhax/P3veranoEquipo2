0abf3a013e6a8b2399339652bcec8d4f8ca7103d
Marcos Cavicchia <antradabha@gmail.com>
2025-01-30 21:09:51 -0300

    Correciones Act1 Class4

--- src/clase4/Main.java
+++ src/clase4/Main.java
@@ -1,24 +1,32 @@
 package clase4;
 
+import clase4.Actividad1.Actividad1;
+import clase4.Actividad1.Cliente;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // ACTIVIDAD 1
         List<Cliente> clientes = new ArrayList<>();
 
-        clientes.add(new Cliente(1,"Pepe", 99));
-        clientes.add(new Cliente(2,"Pedro", 11));
-        clientes.add(new Cliente(3,"George", 120));
-        clientes.add(new Cliente(4,"Floyd", 111));
-        clientes.add(new Cliente(5,"Propylon", 76));
-        clientes.add(new Cliente(6,"LinkAI", 29));
-        clientes.add(new Cliente(7,"Jose", 38));
-        clientes.add(new Cliente(8,"Alfredo", 65));
+        clientes.add(new Cliente(1, "Pepe", 99));
+        clientes.add(new Cliente(2, "Pedro", 11));
+        clientes.add(new Cliente(3, "George", 120));
+        clientes.add(new Cliente(4, "Floyd", 111));
+        clientes.add(new Cliente(5, "Propylon", 76));
+        clientes.add(new Cliente(6, "LinkAI", 29));
+        clientes.add(new Cliente(7, "Jose", 38));
+        clientes.add(new Cliente(8, "Alfredo", 65));
 
         Actividad1 actividad1 = new Actividad1();
 
-        Cliente mejor = actividad1.mejorScoring(clientes,0, clientes.size()-1);
+        Cliente mejor = actividad1.mejorScoring(clientes, 0, clientes.size() - 1);
         System.out.println(mejor.getScoring());
+        // ################################### //
+
+        // ACTIVIDAD 2
+
     }
 }
